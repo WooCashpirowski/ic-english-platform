11fee5525557a8fcf39975b056c4d00e66bc8e1c
Woo Cashpirowski <cashpirowski@gmail.com>
2020-08-14 11:12:51 +0200

    store isTrainer state in localStorage

--- src/context/context.js
+++ src/context/context.js
@@ -1,13 +1,20 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  const [isTrainer, setIsTrainer] = useState(false);
+  const trainerState = localStorage.getItem("isTrainer")
+    ? JSON.parse(localStorage.getItem("isTrainer"))
+    : [false];
+  const [isTrainer, setIsTrainer] = useState(trainerState);
   const setTrainer = (bool) => {
     setIsTrainer(bool);
     return isTrainer;
   };
+  useEffect(() => {
+    localStorage.setItem("isTrainer", JSON.stringify(isTrainer));
+  }, [isTrainer]);
+
   return (
     <AppContext.Provider value={{ isTrainer, setTrainer }}>
       {children}
